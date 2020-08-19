bc3569fbb97bd72133be043b813cba180318bcb1
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2020-08-20 01:02:06 +0200

    added classnames as dependency

--- src/mergeClassNames.js
+++
@@ -1,6 +0,0 @@
-export default function mergeClassNames (className, ...args) {
-  const joinArgs = args.join(' ')
-  const finalClassName = className ? joinArgs.concat(' ', className) : joinArgs
-
-  return finalClassName
-}
